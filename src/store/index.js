b4b88417336a83436f96e777c23de85bdd00ddd4
r_cho_koryo <zhanghl_116@r.recruit.co.jp>
2017-03-01 00:44:55 +0900

    init

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+// @flow
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from '../reducers';
+
+
+const middleWare = [thunk];
+
+const createStoreWithMiddleware = applyMiddleware(...middleWare)(createStore);
+
+const store = createStoreWithMiddleware(reducers);
+
+export default store;
